f51c4e20fa2e217c4ff9d8afa546a7099949ad6b
gregchris <gregchris07258305@gmail.com>
2025-11-17 18:01:53 +0900

    asdf

---
+++ codes/05_ifs.py
@@ -0,0 +1,15 @@
+first=4
+# if first >= 5 :
+#     print("welcom to the rollercoaster!")
+#     print("it's me")
+
+second=6
+
+first = int(input("input fist number: "))
+second = int(input("input second number: "))
+
+if first >= second :
+    print("welcom to the rollercoaster!")
+    print("it's me")
+
+print("End of promgram.")
